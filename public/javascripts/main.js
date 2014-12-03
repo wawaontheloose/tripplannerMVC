aa87cfd0d0c56d54b3ca14547d1755b41820a6ef
Omri Bernstein <hippiccolo@gmail.com>
2014-12-03 10:12:24 -0500

    First pass ready

--- public/javascripts/main.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	$('#day-panel').html(_.template($('#day_panel_template').html())({dayNum: 1}));
-	$('#day-btn-container').append(_.template($('#button_template').html())({dayNum: 1}));
-	['hotels', 'restaurants', 'thingsToDo'].forEach(function(aType) {
-		$('.choose.' + aType).append(_.template($('#dropdown_template').html())());
-		$('.choose.' + aType + ' ul').append(_.template($('#option_template').html())({name: 'testing ' + aType}));
-		$('.list-group.' + aType).append(_.template($('#activity_template').html())({name: 'other testing ' + aType}));
-	});
-});
